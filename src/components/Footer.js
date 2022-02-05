99647ca7c3ec3b2ff7eff3b1ccbab677245cebfa
vanhuycx <vanhuycx@gmail.com>
2022-02-05 08:43:08 -0500

    Edit the footer for name change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -13,7 +13,7 @@ const Footer = () => {
         </div>
 
 
-          <p>Coindex provides crypto exchange information. In addition to tracking the price, Coindex also offers real time news. While using this site, you agree to have accepted our terms of use, cookie and privacy policy.</p>
+          <p>CoinChain provides crypto exchange information. In addition to tracking the price, CoinChain also offers real time news. While using this site, you agree to have accepted our terms of use, cookie and privacy policy.</p>
           <p> Copyright &copy; 2022 <script> document.write(new Date().getFullYear())</script> CoinChain All Rights Reserved</p>
 
     </div>
