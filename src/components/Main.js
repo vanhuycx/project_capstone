5e7772388ac987e4228ffbdd29331d80573d1a4d
vanhuycx <vanhuycx@gmail.com>
2022-02-09 17:51:48 -0500

    Update the code in main component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,14 @@
 import React from "react";
-import {useGetGlobalStatsQuery} from '../apiServices/cryptoApi'
+// import {useGetGlobalStatsQuery} from '../apiServices/cryptoApi'
 
 const Main = () => {
-  const {data,isFetching} = useGetGlobalStatsQuery();
 
-  console.log(data)
-
-  if (isFetching) return '....Loading'
   
 
   return (
     <>
     <div>
-      Market cap: {data?.data?.total_market_cap?.usd ? data?.data.total_market_cap?.usd : 'No data' }
+      This is a Main content
     </div>
 
     </>
