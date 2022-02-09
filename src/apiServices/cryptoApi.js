2897613a07003810b0928643ac2283f440c5b244
vanhuycx <vanhuycx@gmail.com>
2022-02-09 16:20:29 -0500

    Create cryptoAPI

---
+++ src/apiServices/cryptoApi.js
@@ -0,0 +1,33 @@
+import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
+
+
+//For Coingecko
+const cryptoApiHeaderCoingecko = { 
+    'x-rapidapi-host': process.env.REACT_APP_COINGECKO_RAPIDAPI_HOST,
+    'x-rapidapi-key':  process.env.REACT_APP_RAPIDAPI_API_KEY,
+}
+
+const baseUrlCoingecko = 'https://coingecko.p.rapidapi.com';
+
+const createRequest = (url)=> ({url,headers:cryptoApiHeaderCoingecko});
+
+export const cryptoApiCoingecko = createApi({
+    reducerPath: 'cryptoApiCoingecko',
+    baseQuery: fetchBaseQuery({baseUrl:baseUrlCoingecko}),
+    endpoints: (builder)=>({
+        getCryptos: builder.query({
+            query: ({page,per_page}) => createRequest(`/coins/markets?vs_currency=usd&page=${page}&per_page=${per_page}`),
+        }),
+    })
+})
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApiCoingecko;
+
+
+
+
+
+
+
