fc8a9f4d4f60836e8282819f9c86a39034442b69
vanhuycx <vanhuycx@gmail.com>
2022-02-12 10:24:48 -0500

    Change the layout of the website

--- src/App.js
+++ src/App.js
@@ -4,23 +4,20 @@ import Main from "./components/Main";
 import Footer from "./components/Footer";
 import './App.css'
 
-
-
 const App = () => {
   return (
         <>
         <div className="app">
-
             <div className="navbar">
                 <Navbar />
             </div>
 
             <div className="main">
                 <Main/>
-            </div>
 
-            <div className="footer">
-                <Footer/>
+                <div className="footer">
+                    <Footer/>
+                </div>
             </div>
 
         </div>
