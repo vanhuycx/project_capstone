50f954ef1f2c2f83722c6f63fcce981d43ae86d3
vanhuycx <vanhuycx@gmail.com>
2022-02-12 14:09:50 -0500

    Comment out API data

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,18 @@
 import React from "react";
-import {useGetGlobalStatsQuery} from '../apiServices/cryptoApi'
+// import {useGetGlobalStatsQuery} from '../apiServices/cryptoApi'
 
 const Main = () => {
-  const {data:globalStats,isFetching} =  useGetGlobalStatsQuery()
-  console.log(globalStats)
+  // const {data:globalStats,isFetching} =  useGetGlobalStatsQuery()
+  // console.log(globalStats)
 
   
-  const active_cryptocurrencies = globalStats?.data?.active_cryptocurrencies
-  const total_market_cap = globalStats?.data?.market_cap
-  const total_volume = globalStats?.data?.total_volume?.usd
+  // const active_cryptocurrencies = globalStats?.data?.active_cryptocurrencies
+  // const total_market_cap = globalStats?.data?.market_cap
+  // const total_volume = globalStats?.data?.total_volume?.usd
 
   return (
     <>
-      <h1>Active cryptocurrencies: {active_cryptocurrencies} </h1>
+      {/* <h1>Active cryptocurrencies: {active_cryptocurrencies} </h1> */}
       
 
     </>
