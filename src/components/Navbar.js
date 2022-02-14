cbc80750e51014808a81c2de0a166d607ed540e4
vanhuycx <vanhuycx@gmail.com>
2022-02-13 21:43:41 -0500

    Add response for onClick menu bars button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,33 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import {HomeOutlined, MoneyCollectOutlined, BulbOutlined,FundOutlined, MenuOutlined} from '@ant-design/icons'
+import { Button } from "antd";
 import moon_icon from '../moon_icon.svg'
 
 
 const Navbar = () => {
+
+  // const [activeMenu, setActiveMenu] = useState(true);
+  // const [screenSize, setScreenSize] = useState(null)
+
+  // useEffect(() => {
+  //   const handleResize = () => setScreenSize(window.innerWidth);
+
+  //   window.addEventListener('resize',handleResize);
+
+  //   handleResize();
+
+  //   return () => window.removeEventListener('resize',handleResize)
+  // }, [])
+
+  // useEffect(() => {
+    
+  //     if (screenSize<700) {
+  //         setActiveMenu(false)
+  //     } else {
+  //         setActiveMenu(true)
+  //     }
+  // }, [screenSize])
+  
 
   return (
       <>
@@ -15,16 +39,25 @@ const Navbar = () => {
               CoinChain
             </div>
                      
-        </div>
+        </div>   
+
+        {/* {activeMenu && ( */}
+          <div className="navbar-container">     
+            <a href="/"><HomeOutlined/> Home</a>
+            <a href="/"><MoneyCollectOutlined/> Cryptocurrencies</a>
+            <a href="/"><BulbOutlined/> Exchanges</a>
+            <a href="/"><FundOutlined/> News</a>         
+          </div>
+        {/* )}    */}
       
-        <div className="navbar-container">     
-          <a href="/"><HomeOutlined/> Home</a>
-          <a href="/"><MoneyCollectOutlined/> Cryptocurrencies</a>
-          <a href="/"><BulbOutlined/> Exchanges</a>
-          <a href="/"><FundOutlined/> News</a>
-          
-        </div>
-        <MenuOutlined className="menu-bars"/>
+        
+
+        {/* <Button className="menu-bars" onClick={()=>setActiveMenu(!activeMenu)}>
+
+          <MenuOutlined />
+        </Button>
+         */}
+       
       </>
     )
 };
