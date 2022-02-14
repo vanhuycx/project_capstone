d8e50f57a0ca301efb7b21d7fa529d53fb37c37d
vanhuycx <vanhuycx@gmail.com>
2022-02-14 10:25:59 -0500

    Clean up the Navbar file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,33 +1,10 @@
-import React,{useEffect,useState} from "react";
+import React from "react";
 import {HomeOutlined, MoneyCollectOutlined, BulbOutlined,FundOutlined, MenuOutlined} from '@ant-design/icons'
-import { Button } from "antd";
 import moon_icon from '../moon_icon.svg'
 
 
 const Navbar = () => {
 
-  // const [activeMenu, setActiveMenu] = useState(true);
-  // const [screenSize, setScreenSize] = useState(null)
-
-  // useEffect(() => {
-  //   const handleResize = () => setScreenSize(window.innerWidth);
-
-  //   window.addEventListener('resize',handleResize);
-
-  //   handleResize();
-
-  //   return () => window.removeEventListener('resize',handleResize)
-  // }, [])
-
-  // useEffect(() => {
-    
-  //     if (screenSize<700) {
-  //         setActiveMenu(false)
-  //     } else {
-  //         setActiveMenu(true)
-  //     }
-  // }, [screenSize])
-  
 
   return (
       <>
@@ -41,22 +18,16 @@ const Navbar = () => {
                      
         </div>   
 
-        {/* {activeMenu && ( */}
+
           <div className="navbar-container">     
             <a href="/"><HomeOutlined/> Home</a>
             <a href="/"><MoneyCollectOutlined/> Cryptocurrencies</a>
             <a href="/"><BulbOutlined/> Exchanges</a>
             <a href="/"><FundOutlined/> News</a>         
           </div>
-        {/* )}    */}
       
         
 
-        {/* <Button className="menu-bars" onClick={()=>setActiveMenu(!activeMenu)}>
-
-          <MenuOutlined />
-        </Button>
-         */}
        
       </>
     )
