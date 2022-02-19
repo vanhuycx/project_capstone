9063f48a3cd58a75f8da2f7b1be22f6d59d7fae1
vanhuycx <vanhuycx@gmail.com>
2022-02-19 13:56:49 -0500

    Add responsive Menu bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {HomeOutlined, MoneyCollectOutlined, BulbOutlined,FundOutlined, MenuOutlined} from '@ant-design/icons'
+import { Button } from "antd";
 import moon_icon from '../moon_icon.svg'
 import { Link } from "react-router-dom";
+import useMediaQuery from '@mui/material/useMediaQuery';
+
 
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true)
+  const bigScreen = useMediaQuery('(min-width:750px)')
 
-
+  useEffect(() => {
+    if (bigScreen) {
+      setActiveMenu(true)
+    } else {
+      setActiveMenu(false)
+    }
+  }, [bigScreen])
+  
+  
   return (
       <>
         <div className="header-container">
@@ -17,16 +30,21 @@ const Navbar = () => {
             </div>
                      
         </div>   
+
+          {activeMenu&&
+
           <div className="navbar-container">     
             <Link to='/'><HomeOutlined/> Home</Link>
             <Link to='/cryptocurrencies'><MoneyCollectOutlined/> Cryptocurrencies</Link>
             <Link to='/exchanges'><BulbOutlined/> Exchanges</Link>
             <Link to='/news'><FundOutlined/> News</Link>
           </div>
-      
-        
-
-       
+          }        
+        <div className="menu-bars">  
+            <Button onClick={()=>setActiveMenu(!activeMenu)}>
+              <MenuOutlined/>
+            </Button>    
+        </div>
       </>
     )
 };
