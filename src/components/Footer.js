3f24d1d36862c6fd8a90b888f0e7ecd0101a1538
vanhuycx <vanhuycx@gmail.com>
2022-02-20 08:43:14 -0500

    Add Link to Footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
@@ -6,10 +7,10 @@ const Footer = () => {
     <div className="footer-container">
 
         <div>
-          <a href="/">Home</a>
-          <a href="/">CryptoCurrencies</a>
-          <a href="/">Exchanges</a>
-          <a href="/">News</a><br/> <br />
+            <Link to='/'> Home</Link>
+            <Link to='/cryptocurrencies'> Cryptocurrencies</Link>
+            <Link to='/exchanges'> Exchanges</Link>
+            <Link to='/news'> News</Link>
         </div>
 
           <p>CoinChain provides crypto exchange information. In addition to tracking the price, CoinChain also offers real time news. While using this site, you agree to have accepted our terms of use, cookie and privacy policy.</p>
