a4fcefda88321e3dd609918e24a61b6b8fd2ce60
vanhuycx <vanhuycx@gmail.com>
2022-03-01 17:45:06 -0500

    Testing with CORS

--- src/apiServices/newsApi.js
+++ src/apiServices/newsApi.js
@@ -5,16 +5,21 @@ const baseUrlNews = 'https://news.google.com/rss';
 
 export const newsApi = createApi({
     reducerPath: 'newsApi',
-    baseQuery: fetchBaseQuery({baseUrl:baseUrlNews}),
+    baseQuery: fetchBaseQuery({baseUrl:baseUrlNews,
+        prepareHeaders:(headers)=> {
+            headers.set('Access-Control-Allow-Origin','*')  
+            headers.set('Access-Control-Allow-Methods','GET,POST,OPTIONS,DELETE,PUT')  
+            headers.set('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token')  
+            headers.set('Access-Control-Allow-Credentials',true)
+            return headers
+            }
+        }
+    ),
     endpoints: (builder)=>({
         getNews: builder.query({
             query: () => ({url:'/search?q=cryptocurrency'}),
         }),
     }),
-    prepareHeaders: (headers) => {
-    headers.set('Access-Control-Allow-Origin', '*')
-    return headers
-    }
 })
 
 export const {
