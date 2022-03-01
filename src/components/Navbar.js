281246dde7281613b182ddbfce372a37b80484aa
vanhuycx <vanhuycx@gmail.com>
2022-03-01 15:46:44 -0500

    Rebuild the Menubars as Dropdown

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React,{useState,useEffect} from "react";
 import {HomeOutlined, MoneyCollectOutlined, BulbOutlined,FundOutlined, MenuOutlined} from '@ant-design/icons'
-import { Button,Space } from "antd";
+import { Button,Space,Dropdown } from "antd";
 import moon_icon from '../moon_icon.svg'
 import { Link } from "react-router-dom";
 
 import useMediaQuery from '@mui/material/useMediaQuery';
 
+
+const menu = (
+  <>
+    <div className="navbar-container">    
+      <Link to='/'><HomeOutlined/> Home</Link>
+      <Link to='/cryptocurrencies'><MoneyCollectOutlined/> Cryptocurrencies</Link>
+      <Link to='/exchanges'><BulbOutlined/> Exchanges</Link>
+      <Link to='/news'><FundOutlined/> News</Link>
+    </div>
+  </>
+);
 
 const Navbar = () => {
   const [activeMenu, setActiveMenu] = useState(true)
@@ -19,7 +30,6 @@ const Navbar = () => {
     }
   }, [bigScreen])
   
-  
   return (
       <>
         <div className="header-container">
@@ -32,19 +42,24 @@ const Navbar = () => {
           </Space>
         </div>   
 
-          {activeMenu &&
-          <div className="navbar-container">     
-            <Link to='/'><HomeOutlined/> Home</Link>
-            <Link to='/cryptocurrencies'><MoneyCollectOutlined/> Cryptocurrencies</Link>
-            <Link to='/exchanges'><BulbOutlined/> Exchanges</Link>
-            <Link to='/news'><FundOutlined/> News</Link>
-          </div>
+          {activeMenu 
+            ? menu 
+            : 
+            <Dropdown className="menu-bars" overlay={menu}>
+              <Button className="ant-dropdown-link">
+                <MenuOutlined/>
+              </Button>
+            </Dropdown>
           }        
-        <div className="menu-bars">  
+
+
+        {/* <div className="menu-bars">  
             <Button onClick={()=>setActiveMenu(!activeMenu)}>
               <MenuOutlined/>
             </Button>    
-        </div>
+        </div> */}
+
+        
       </>
     )
 };
