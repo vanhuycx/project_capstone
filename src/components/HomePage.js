61f8f91246966c1bf1138214bbba30c07b963fd8
vanhuycx <vanhuycx@gmail.com>
2022-03-06 21:15:54 -0500

    Use prefix for statistic instead of localeString function to set the number of precision

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -30,7 +30,7 @@ const HomePage = () => {
 
         <Statistic title='Market Cap Percentage 24h Change' precision={2} suffix='%' value={globalStats?.market_cap_change_percentage_24h_usd || 'No data'}/>
           
-          <Statistic title='24h Volume' prefix='$' value={globalStats?.total_volume?.usd.toLocaleString("en-US",{maximumFractionDigits: 2}) || 'No data'}/>
+          <Statistic title='24h Volume' prefix='$' precision={2} value={globalStats?.total_volume?.usd || 'No data'}/>
 
         <Statistic title='Dominance' value={('BTC: ' + globalStats?.market_cap_percentage?.btc.toLocaleString("en-US",{maximumFractionDigits: 2}) + '% - ETH: ' + globalStats?.market_cap_percentage?.eth.toLocaleString("en-US",{maximumFractionDigits: 2}) + '%') || 'No data'}/>
 
