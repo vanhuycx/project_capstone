a3b758df0c02021c1f5bc2386e64e33c69983ad6
vanhuycx <vanhuycx@gmail.com>
2022-03-16 11:48:23 -0400

    Adjust navbar responsive font-size

--- src/components/News.js
+++ src/components/News.js
@@ -5,15 +5,8 @@ import { useGetCryptosQuery } from "../apiServices/cryptoApi";
 const News = () => {
   const {data:news,isFetching} = useGetNewsQuery()
 
-
     console.log(news?.articles)
 
-
-  
-  const {data:cryptos,isFetching:fetchCryptos} = useGetCryptosQuery({page:1,per_page:10});
-  console.log(cryptos)
-
-  if (fetchCryptos) return '...Loading'
 
   if (isFetching) return '...Loading'
 
@@ -21,10 +14,6 @@ const News = () => {
     <div className="content-wrapper">
       <h1>This is the News page</h1>
 
-
-      <h1>This is the Cryptocurrencies page</h1>
-
-{cryptos.map(coin=><h4>{coin.current_price}</h4>)}
     </div>
 
     
