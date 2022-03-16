f769c914d982132e10c0c0688ab2f168ad1ea7a9
vanhuycx <vanhuycx@gmail.com>
2022-03-16 08:48:50 -0400

    Add the index.css file and normalize the css code for the body and code tag

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css'
 import 'antd/dist/antd.css';
 import store from './apiServices/store/store'
 import { Provider } from 'react-redux'
