e9f431ff90e29a225133b5c612cfab5983b60544
vanhuycx <vanhuycx@gmail.com>
2022-03-19 22:06:24 -0400

    Add content for Notfound page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,17 +1,29 @@
 import React from 'react'
 import { Link } from "react-router-dom"
-
+import BitMan from '../image/BitMan.jpg'
 
 const NotFound = () => {
   return (
 
     <>
+    <div className="notfound">
 
-    <div>
-        Sorry. We cannot find the page you want.
+      <div>
+          Sorry. We cannot find the page you want.
+      </div>
+
+      <p>Go to <Link to='/'>home page</Link></p>
+
+      <div>
+            <img  alt='' src={BitMan} />
+      </div>
+
+
     </div>
 
-    <Link to='/'>Home</Link>
+    
+
+   
     
     
     </>
