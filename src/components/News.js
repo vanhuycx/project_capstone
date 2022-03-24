6e09773a32aeeb2b7346f947f0b6b27994e4442f
vanhuycx <vanhuycx@gmail.com>
2022-03-24 09:49:44 -0400

    Adjust news section format style

--- src/components/News.js
+++ src/components/News.js
@@ -15,40 +15,41 @@ const News = () => {
 
   const uniqueArticles = Array.from(
     new Set(news?.articles?.map((article) => article.title))
-  ).map((title) => news?.articles.find((article) => article.title === title))
-
+  ).map((title) => news?.articles.find((article) => article.title === title));
 
   if (isFetching) return '...Loading';
 
   return (
-      <Space>
-        <List
-          grid={{ gutter: 16, xs: 2, sm: 3, md: 3, lg: 4, xl: 5, xxl: 5 }}
-          dataSource={uniqueArticles}
-          renderItem={(item) => (
-            <List.Item>
-              <a href={item?.link} rel='noreferrer noopener' target='_blank'>
-                <Card
-                  hoverable
-                  style={{ width: 220 }}
-                  cover={
-                    <img width={70} height={140} src={item?.media} alt='' /> ||
-                    ''
-                  }
-                >
-                  <h3>{item?.title}</h3>
-                  <p>{item?.summary?.slice(0, 100) + '...' || ''}</p>
+    <Space>
+      <List
+        grid={{ gutter: 16, xs: 1, sm: 2, md: 3, lg: 4, xl: 5, xxl: 6 }}
+        dataSource={uniqueArticles}
+        renderItem={(item) => (
+          <List.Item>
+            <a href={item?.link} rel='noreferrer noopener' target='_blank'>
+              <Card
+                className='news-card'
+                hoverable
+                style={{ width:220 }}
+                cover={
+                  
+                 
+                  <img className='news-image' height={120} src={item?.media} alt='' /> || ''
+                }
+              >
+                <h3>{item?.title}</h3>
+                <p>{item?.summary?.slice(0, 100) + '...' || ''}</p>
 
-                  <Meta
-                    title={item?.authors || ''}
-                    description={item?.published_date}
-                  />
-                </Card>
-              </a>
-            </List.Item>
-          )}
-        />
-      </Space>
+                <Meta
+                  title={item?.authors || ''}
+                  description={item?.published_date}
+                />
+              </Card>
+            </a>
+          </List.Item>
+        )}
+      />
+    </Space>
   );
 };
 
