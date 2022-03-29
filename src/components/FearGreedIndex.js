aa51eac716eb5524f0382d67ea4ee803cbdc61fd
vanhuycx <vanhuycx@gmail.com>
2022-03-29 10:46:17 -0400

    Add FearGreedIndex component

---
+++ src/components/FearGreedIndex.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useGetFearGreedIndexQuery } from '../apiServices/fearGreedApi';
+
+const FearGreedIndex = () => {
+  const { data: fearGreedIndex } = useGetFearGreedIndexQuery({ limit: 10 });
+
+  console.log(fearGreedIndex);
+  return (
+    <>
+      <div>FearGreedIndex</div>; c
+    </>
+  );
+};
+
+export default FearGreedIndex;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -9,6 +9,7 @@ import News from './News';
 import Cryptocurrencies from './Cryptocurrencies';
 import { Link } from 'react-router-dom';
 import Loader from '../utils/Loader';
+import { useGetFearGreedIndexQuery } from '../apiServices/fearGreedApi';
 
 const { Title } = Typography;
 
@@ -22,6 +23,8 @@ const HomePage = () => {
   const { data: trending, isFetching: isTrendingFetching } =
     useGetTrendingQuery({ pollingInterval: 300000 });
   const trendingCoins = trending?.coins;
+
+
 
   const LastUpdate = new Date(globalStats?.updated_at * 1000);
 
