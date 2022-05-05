5d194193a4c57c2c63a09eee233e687d7219835b
vanhuycx <vanhuycx@gmail.com>
2022-05-05 00:24:50 -0400

    Impelement emailing in contact us section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
+import emailjs from '@emailjs/browser';
 
 const Footer = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        'service_fg238fj',
+        'template_a69idfa',
+        form.current,
+        '79UeZVyL0qp4iSTTP'
+      )
+      .then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+    form.current.reset();
+  };
   return (
     <>
       <div className='footer-container'>
@@ -12,20 +35,8 @@ const Footer = () => {
           <Link to='/news'> News</Link>
         </div>
 
-        <p>
-          CoinChain provides crypto exchange information. In addition to
-          tracking the price, CoinChain also offers real time news. While using
-          this site, you agree to have accepted our terms of use, cookie and
-          privacy policy.
-        </p>
-        <p>
-          Copyright &copy; {new Date().getFullYear()} CoinChain All Rights
-          Reserved
-        </p>
-
-        <h1>Contact Us</h1>
-
-        <div className='container'>
+        <div className='contact-container'>
+          <h1>Contact Us</h1>
           <form ref={form} onSubmit={sendEmail}>
             <label>Name</label>
             <input type='text' name='user_name' placeholder='Your name...' />
@@ -40,6 +51,16 @@ const Footer = () => {
             <input type='submit' value='Submit' />
           </form>
         </div>
+        <p>
+          CoinChain provides crypto exchange information. In addition to
+          tracking the price, CoinChain also offers real time news. While using
+          this site, you agree to have accepted our terms of use, cookie and
+          privacy policy.
+        </p>
+        <p>
+          Copyright &copy; {new Date().getFullYear()} CoinChain All Rights
+          Reserved
+        </p>
       </div>
     </>
   );
